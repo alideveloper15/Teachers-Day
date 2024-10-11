baee685d9d579d28795cb70b51f7161de1e84536
Muhammad Ali Siddiqui <muhammadalisiddiqui95@gmail.com>
2024-10-12 02:22:47 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Hello! I am AliDeveloper.");
+alert("Happy Teachers Day!");
+alert("Especially The best teacher of my life");
+alert("Respected Sir Rizwan and Sir Asad");
+alert("Thank you sir for all your hardwork on us!");
